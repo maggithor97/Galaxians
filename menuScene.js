139297fead2a1157a82494316aad6f7bd5e37e7e
Ívar Árnason <ivar1309@gmail.com>
2021-11-18 15:24:24 +0000

    Add: Charger score for boss (yellow) now randomizes at start

--- menuScene.js
+++ menuScene.js
@@ -7,22 +7,31 @@ g_menuScene.init = function() {
   
   this.animationInterval = SECS_TO_NOMINALS;
   this.animationTimer = 0;
+  this.bossRandomScoreTimer = 0;
+
+  this.ready = false;
 };
 
 g_menuScene.updateSimulation = function (du) {
   this.animationTimer += du;
+  this.bossRandomScoreTimer += du;
 
   background.update(du);
 
   hud.update(du);
 
-  if (eatKey(KEY_SPACE)) {
+  if (this.ready && eatKey(KEY_SPACE)) {
     g_sceneManager.loadScene("game");
   }
 };
 
 g_menuScene.renderSimulation = function (ctx) {
   background.render(ctx);
+
+  if (this.bossRandomScoreTimer > 0.25 * this.animationInterval && !this.ready) {
+    scoreManager.setRandomBossScore();
+    this.bossRandomScoreTimer = 0;
+  }
 
   if (this.animationTimer > 2 * this.animationInterval) {
     Text(ctx, "WE ARE THE GALAXIANS", 90, 150, g_fonts.red);
@@ -44,8 +53,9 @@ g_menuScene.renderSimulation = function (ctx) {
     //let alien = entityManager.getAlien(4).sprite.drawCentredAt(ctx, 90, 290);
     drawCentredAt(ctx, 90, 290, 4);
     
-    Text(ctx, "60", 145, 290, g_fonts.blue);
-    Text(ctx, "200", 240, 290, g_fonts.blue);
+    //Text(ctx, "60", 145, 290, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("convoy", 3).toString(), 145, 290, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("charger", 3).toString(), 240, 290, g_fonts.blue);
 
     Text(ctx, "PTS", 300, 290, g_fonts.blue);
   }
@@ -55,8 +65,8 @@ g_menuScene.renderSimulation = function (ctx) {
     //let alien = entityManager.getAlien(3).sprite.drawCentredAt(ctx, 90, 320);
     drawCentredAt(ctx, 90, 320, 1);
     
-    Text(ctx, "50", 145, 320, g_fonts.blue);
-    Text(ctx, "100", 240, 320, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("convoy", 0).toString(), 145, 320, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("charger", 0).toString(), 240, 320, g_fonts.blue);
     Text(ctx, "PTS", 300, 320, g_fonts.blue);
   }
 
@@ -64,8 +74,8 @@ g_menuScene.renderSimulation = function (ctx) {
     //let alien = entityManager.getAlien(2).sprite.drawCentredAt(ctx, 90, 350);
     drawCentredAt(ctx, 90, 350, 2);
     
-    Text(ctx, "40", 145, 350, g_fonts.blue);
-    Text(ctx, "80", 250, 350, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("convoy", 1).toString(), 145, 350, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("charger", 1).toString(), 250, 350, g_fonts.blue);
 
     Text(ctx, "PTS", 300, 350, g_fonts.blue);
   }
@@ -74,13 +84,15 @@ g_menuScene.renderSimulation = function (ctx) {
     //let alien = entityManager.getAlien(1).sprite.drawCentredAt(ctx, 90, 380);
     drawCentredAt(ctx, 90, 380, 3);
     
-    Text(ctx, "30", 145, 380, g_fonts.blue);
-    Text(ctx, "60", 250, 380, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("convoy", 2).toString(), 145, 380, g_fonts.blue);
+    Text(ctx, scoreManager.getFromScoreTable("charger", 2).toString(), 250, 380, g_fonts.blue);
 
     Text(ctx, "PTS", 300, 380, g_fonts.blue);
   }
 
   if (this.animationTimer > 11 * this.animationInterval) {
+    this.ready = true;
+
     Text(ctx, "PRESS", 100, 430, g_fonts.white);
     Text(ctx, " SPACE ", 166, 430, g_fonts.blue);
     Text(ctx, "TO PLAY", 258, 430, g_fonts.white);
